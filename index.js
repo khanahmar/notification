f6f5f717e9f145cf08482aa3abbc90986e946514
Ahmar khan <ahmarbilal223344@gmail.com>
2023-01-28 00:07:44 +0500

    done

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const btn = document.querySelector(".btn")
 const notifyEl = document.querySelector(".notify")
 let message = 0
-
-let color = generateColor()
-
+let color
 function generateColor() {
   let randColor = 0xffffff
-  return "#" + toString(Math.floor(Math.random() * randColor))
+  return (
+    "#" +
+    Math.floor(Math.random() * randColor)
+      .toString(16)
+      .toLocaleUpperCase()
+  )
 }
 
 console.log(color)
 
 btn.addEventListener("click", (e) => {
+  color = generateColor()
   const divEl = document.createElement("div")
   divEl.classList.add("text")
+  divEl.style.color = color
   divEl.innerHTML = `Message ${message++}`
   notifyEl.appendChild(divEl)
   setTimeout(() => {
